77325e4b67fbc8bdd28f0441c67a6697c62290bf
git.sujalsingh@gmail.com <git.sujalsingh@gmail.com>
2026-06-24 12:57:39 +0530

    fixed svg path

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -7,28 +7,28 @@ const Docs = ({windowState, setWindowState}) => {
     <footer className='dock'>
         <div 
         onClick={()=>{setWindowState(state=>({...state,github:true}))}}
-         className="icon github"> <img src="/doc-icons/github.svg" alt="" /></div>
+         className="icon github"> <img src={`${import.meta.env.BASE_URL}doc-icons/github.svg`} alt="" /></div>
         <div 
         onClick={()=>{setWindowState(state=>({...state,note:true}))}}
-         className="icon note"><img src="/doc-icons/note.svg" alt="" /></div>
+         className="icon note"><img src={`${import.meta.env.BASE_URL}doc-icons/note.svg`} alt="" /></div>
         <div 
         onClick={()=>{window.open("mailto:sujal@example.com","_blank")}}
-         className="icon mail"><img src="/doc-icons/mail.svg" alt="" /></div>
+         className="icon mail"><img src={`${import.meta.env.BASE_URL}doc-icons/mail.svg`} alt="" /></div>
         <div 
         onClick={()=>{window.open("https://calendar.google.com/calendar/u/0/r")}}
-         className="icon calender"><img src="/doc-icons/calender.svg" alt="" /></div>
+         className="icon calender"><img src={`${import.meta.env.BASE_URL}doc-icons/calender.svg`} alt="" /></div>
         <div 
         onClick={()=>{window.open("https://www.ilovepdf.com/jpg_to_pdf", "_blank")}}
-         className="icon pdf"><img src="/doc-icons/pdf.svg" alt="" /></div>
+         className="icon pdf"><img src={`${import.meta.env.BASE_URL}doc-icons/pdf.svg`} /></div>
         <div 
         onClick={()=>{setWindowState(state=>({...state,spotify:true}))}}
-         className="icon spotify"><img src="/doc-icons/spotify.svg" alt="" /></div>
+         className="icon spotify"><img src={`${import.meta.env.BASE_URL}doc-icons/spotify.svg`} alt="" /></div>
         <div 
         onClick={()=>{window.open("https://www.linkedin.com/in/sujal-singh-65148b369" ,"_blank")}}
-         className="icon link"><img src="/doc-icons/link.svg" alt="" /></div>
+         className="icon link"><img src={`${import.meta.env.BASE_URL}doc-icons/link.svg`} alt="" /></div>
         <div 
         onClick={()=>{setWindowState(state=>({...state,cli:true}))}}
-         className="icon cli"><img src="/doc-icons/cli.svg" alt="" /></div>
+         className="icon cli"><img src={`${import.meta.env.BASE_URL}doc-icons/cli.svg`} alt="" /></div>
     </footer>
   )
 }
